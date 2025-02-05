1ccde999fe2343722fd0d2445ccc6220ff724132
aarn12343235 <uzumakinrtq@gmail.com>
2025-02-05 21:14:16 +0800

    EEEE

--- Inventory/Program.cs
+++ Inventory/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 namespace GroceryStoreDiscountCalculator
 {
+    
     public class Product
     {
         public string Name { get; set; }
@@ -21,6 +22,7 @@ namespace GroceryStoreDiscountCalculator
         {
             ArrayList cart = new ArrayList(); 
 
+            
             Product apple = new Product("Apple", 1.0);
             Product brush = new Product("Brush", 5.05);
             Product mirror = new Product("Mirror", 3.20);
@@ -29,19 +31,21 @@ namespace GroceryStoreDiscountCalculator
 
             bool continueShopping = true;
 
-            Console.WriteLine("Welcome to the Grocery Store!");
-            Console.WriteLine("-----------------------------");
-
             while (continueShopping)
             {
+                Console.Clear();
+                Console.WriteLine("Welcome to the Grocery Store!");
+                Console.WriteLine("-----------------------------");
+
                 
                 Console.WriteLine("Available Products:");
-                Console.WriteLine($"[1]. {apple.Name} - ${apple.Price:F2}");
-                Console.WriteLine($"[2]. {brush.Name} - ${brush.Price:F2}");
-                Console.WriteLine($"[3]. {mirror.Name} - ${mirror.Price:F2}");
-                Console.WriteLine($"[4]. {pen.Name} - ${pen.Price:F2}");
-                Console.WriteLine($"[5]. {bag.Name} - ${bag.Price:F2}");
+                Console.WriteLine($"1. {apple.Name} - ${apple.Price:F2}");
+                Console.WriteLine($"2. {brush.Name} - ${brush.Price:F2}");
+                Console.WriteLine($"3. {mirror.Name} - ${mirror.Price:F2}");
+                Console.WriteLine($"4. {pen.Name} - ${pen.Price:F2}");
+                Console.WriteLine($"5. {bag.Name} - ${bag.Price:F2}");
 
+               
                 Console.Write("Enter the number of the product you want to purchase: ");
                 int productChoice = int.Parse(Console.ReadLine());
 
@@ -71,7 +75,11 @@ namespace GroceryStoreDiscountCalculator
                 }
 
                 
-                double partialTotal = CalculateTotal(cart);
+                double partialTotal = 0;
+                foreach (Product product in cart)
+                {
+                    partialTotal += product.Price;
+                }
                 Console.WriteLine($"Partial Total: ${partialTotal:F2}");
 
                 
@@ -79,73 +87,68 @@ namespace GroceryStoreDiscountCalculator
                 string response = Console.ReadLine().ToLower();
 
                 if (response != "yes")
+                {
                     continueShopping = false;
-                
+                }
             }
 
             
-            double totalBeforeDiscount = CalculateTotal(cart);
-            double discount = CalculateDiscount(totalBeforeDiscount);
-            double finalAmount = totalBeforeDiscount - discount;
+            double totalBeforeDiscount = 0;
+            foreach (Product product in cart)
+            {
+                totalBeforeDiscount += product.Price;
+            }
 
             
+            double discount = 0;
+            if (totalBeforeDiscount > 500)
+            
+                discount = totalBeforeDiscount * 0.20; // 20% discount
+            
+            else if (totalBeforeDiscount > 200)
+            
+                discount = totalBeforeDiscount * 0.15; // 15% discount
+            
+            else if (totalBeforeDiscount > 100)
+            
+                discount = totalBeforeDiscount * 0.10; // 10% discount
+            
+
+            double finalAmount = totalBeforeDiscount - discount;
+
+            // Display receipt
             Console.Clear();
             Console.WriteLine("Receipt");
             Console.WriteLine("-------");
-            foreach (Product product in cart)
-            {
-                Console.WriteLine($"{product.Name}: ${product.Price:F2}");
-            }
+
+            
 
             Console.WriteLine($"Total Before Discount: ${totalBeforeDiscount:F2}");
             Console.WriteLine($"Discount Applied: ${discount:F2}");
             Console.WriteLine($"Final Amount: ${finalAmount:F2}");
 
+            //new purchase or exit
             Console.Write("Do you want to make a new purchase? (yes/no): ");
             string newPurchaseResponse = Console.ReadLine().ToLower();
 
             if (newPurchaseResponse == "yes")
-                Main(args); 
-           
+            
+                Main(args); // Restart the application (pwede diay ni Whhahahaha)
+            
             else
+            
                 Console.WriteLine("Thank you for shopping with us! Goodbye!");
+            
         }
 
-        
+        // Method to add a product to the cart
         static void AddProductToCart(ArrayList cart, Product product, int quantity)
         {
             for (int i = 0; i < quantity; i++)
+            {
                 cart.Add(product);
-            
+            }
         }
-
-        static double CalculateTotal(ArrayList cart)
-        {
-            double total = 0;
-            foreach (Product product in cart)
-                total += product.Price;
-
-            return total;
-        }
-
-        static double CalculateDiscount(double total)
-        {
-            if (total > 500)
-            
-                return total * 0.20; // 20% discount
-            
-            else if (total > 200)
-            
-                return total * 0.15; // 15% discount
-            
-            else if (total > 100)
-            
-                return total * 0.10; // 10% discount
-            
-            else
-            
-                return 0; // No discount
-            
-        }
+      
     }
 }
